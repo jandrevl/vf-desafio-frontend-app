2b15fe37c2c00d41d808dd0e987f87c95e23471b
Joao Lopes <jandrevl@gmail.com>
2023-04-07 21:40:07 +0100

    top header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
+import TopHeader from './components/TopHeader';
 
 function App() {
   return (
+    <div className="App">
+    <TopHeader></TopHeader>
     <h1>O DESAFIO VISUALFORMA</h1>
+    </div>
+    
   );
 }
 
--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -1,10 +1,26 @@
+import facebookwhite2x from "../img/facebookwhite2x.png";
+import instagramwhite from "../img/instagramwhite.png";
+import magnifiersearchwhite from "../img/magnifiersearchwhite.png"
+
+const myColor = 246970;
+
 const TopHeader = () => {
-    return ( 
-        <div className="top-header">
-            
-        </div>
-     );
+    return (
+        <nav className='top-header'>
+            <a href="https://www.instagram.com/visualforma/">
+                <img src={ instagramwhite } alt="Instagram Icon" height="15" />
+            </a>
+            <a href="https://pt-pt.facebook.com/visualforma/">
+                <img src={ facebookwhite2x } alt="Small Facebook Icon" height="15" />
+            </a>
+            <div className="search">
+            <input type="text" />
+            <div>
+            <img src={ magnifiersearchwhite } alt="Search Magnifier" height="15" />
+            </div>
+            </div>
+        </nav>
+    );
 }
- 
-export default TopHeader
-r;
+
+export default TopHeader;
