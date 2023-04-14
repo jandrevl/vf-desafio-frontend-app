33b380c1699f2886987cfe5e8b11c96e1d161f81
Joao Lopes <jandrevl@gmail.com>
2023-04-14 12:08:07 +0100

    'Documentos' component complete. 'Links Uteis' next.

--- src/components/OfficialDocs.js
+++ src/components/OfficialDocs.js
@@ -1,6 +1,12 @@
 import './OfficialDocs.css';
 import { ReactComponent as LinkArrow } from '../resources/rightarrow.svg';
-import { ReactComponent as FileLines } from '../resources/file-lines-regular.svg';
+import { ReactComponent as FileLines } from '../resources/file-lines-solid.svg';
+import { ReactComponent as FileContract } from '../resources/file-contract-solid.svg';
+import { ReactComponent as FileMoney } from '../resources/file-invoice-dollar-solid.svg';
+import { ReactComponent as FileTable } from '../resources/table-list-solid.svg';
+
+
+
 
 
 const OfficialDocs = () => {
@@ -18,27 +24,53 @@ const OfficialDocs = () => {
             <div className="document-links-collection">
 
                 <div id='card' className="atas-card">
-                    <FileLines fill='#1f5e64' height='112'/>
+                    <div className="doc-icon">
+                        <FileLines fill='#1f5e64' height='112' />
+                    </div>
                     <div className="doctype-and-link">
                         <p>ATAS</p>
-                        <LinkArrow fill='#FFFFFF' height="13"/>
+                        <LinkArrow className="link-arrow" fill='#FFFFFF' height="13" />
                     </div>
                 </div>
 
                 <div id='card' className="editais-card">
-
+                    <div className="doc-icon">
+                        <FileContract fill='#5e9a87' height='112' />
+                    </div>
+                    <div className="doctype-and-link">
+                        <p>EDITAIS</p>
+                        <LinkArrow className="link-arrow" fill='#FFFFFF' height="13" />
+                    </div>
                 </div>
 
                 <div id='card' className="formularios-card">
-
+                    <div className="doc-icon">
+                        <FileMoney fill='#1d7a81' height='112' />
+                    </div>
+                    <div className="doctype-and-link">
+                        <p>FORMULÁRIOS</p>
+                        <LinkArrow className="link-arrow" fill='#FFFFFF' height="13" />
+                    </div>
                 </div>
 
                 <div id='card' className="documentos-card">
-
+                    <div className="doc-icon">
+                        <FileTable fill='#569a76' height='112' />
+                    </div>
+                    <div className="doctype-and-link">
+                        <p>DOCUMENTOS</p>
+                        <LinkArrow className="link-arrow" fill='#FFFFFF' height="13" />
+                    </div>
                 </div>
 
                 <div id='card' className="inscricoes-card">
-
+                    <div className="doc-icon">
+                        <FileLines fill='#efa21b' height='112' />
+                    </div>
+                    <div className="doctype-and-link">
+                        <p>INSCRIÇÕES</p>
+                        <LinkArrow className="link-arrow" fill='#FFFFFF' height="13" />
+                    </div>
                 </div>
 
             </div>
